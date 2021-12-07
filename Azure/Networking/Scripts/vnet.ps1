675e70b846495b5f1bc057e9ef370984f739c02e
kodebot <mani@kodebot.com>
2021-12-07 06:16:25 +0000

    auto commit

--- Azure/Networking/Scripts/vnet.ps1
+++ Azure/Networking/Scripts/vnet.ps1
@@ -1,1 +1,7 @@
-get-module
+$rg = "temp-mani-vnet-rg"
+$location = "eastus"
+
+$vnetName = "temp-mani-vnet1"
+$vnetAddressSpace = "10.0.0.0/22"
+
+$subnet1 = New-AzVirtualNetworkSubnetConfig -Name "temp-mani-subnet1" -AddressPrefix "10.0.0.0/24"
