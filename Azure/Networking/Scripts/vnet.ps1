2554ca5c7c63687afe486710ca8443458ea9d1e7
kodebot <mani@kodebot.com>
2021-12-07 06:43:58 +0000

    auto commit

--- Azure/Networking/Scripts/vnet.ps1
+++ Azure/Networking/Scripts/vnet.ps1
@@ -5,3 +5,11 @@ $vnetName = "temp-mani-vnet1"
 $vnetAddressSpace = "10.0.0.0/22"
 
 $subnet1 = New-AzVirtualNetworkSubnetConfig -Name "temp-mani-subnet1" -AddressPrefix "10.0.0.0/24"
+$subnet2 = New-AzVirtualNetworkSubnetConfig -Name "temp-mani-subnet2" -AddressPrefix "10.0.1.0/24"
+
+New-AzResourceGroup -Name $rg -Location $location
+
+New-AzVirtualNetwork -Name $ventName -Location $loccation -ResourceGroupName $rg -AddressPrefix $vnetAddressSpace -Subnet $subnet1,$subnet2
+
+# remove
+# Remove-AzResourceGroup -Name $rg
