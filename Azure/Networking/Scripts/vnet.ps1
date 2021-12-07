6f01430f41d708c8b6d9ab9c620093e731432370
kodebot <mani@kodebot.com>
2021-12-07 11:09:55 +0000

    auto commit

--- Azure/Networking/Scripts/vnet.ps1
+++ Azure/Networking/Scripts/vnet.ps1
@@ -9,13 +9,23 @@ $subnet2 = New-AzVirtualNetworkSubnetConfig -Name "temp-mani-subnet2" -AddressPr
 
 New-AzResourceGroup -Name $rg -Location $location
 
+# you can directly set the result of New-AzVirtualNetwork to a variable if you want to use VNET
+# further instead of Get-AzVirtualNetwork
 New-AzVirtualNetwork -Name $vnetName -Location $location -ResourceGroupName $rg -AddressPrefix $vnetAddressSpace -Subnet $subnet1,$subnet2
 
 
 # add more subnets after creating VNET
 
-$subnet3 = New-AzVirtualNetworkSubnetConfig -Name "temp-mani-subnet3" -AddressPrefix "10.0.3.0/24"
+$virtualNetwork = Get-AzVirtualNetwork -Name $vnetName
 
+$subnet3 = Add-AzVirtualNetworkSubnetConfig `
+            -Name "temp-mani-subnet3" `
+            -AddressPrefix "10.0.3.0/24" `
+            -VirtualNetwork $virtualNetwork
+
+# commit the changes
+
+$virtualNetwork | Set-AzVirtualNetwork
 
 
 # remove
